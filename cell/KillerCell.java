f946161631518fe428144ade7e869d04cce614ff
Liam <liamtrnr80@gmail.com>
2020-06-07 16:02:15 +1000

    Create KillerCell.java

---
+++ cell/KillerCell.java
@@ -0,0 +1,33 @@
+package cell;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KillerCell extends Cell {
+    
+    private List<KillerCell> cage;
+    private int cageTotal;
+    
+    public KillerCell(int r, int c, int value) {
+        super(r, c, value);
+        cage = new ArrayList<>();
+    }
+    
+    public void setTotal(int total) {
+        this.cageTotal = total;
+    }
+    
+    public void setCage(List<KillerCell> cage) {
+        this.cage = cage;
+    }
+    
+    @Override
+    public boolean isValid() {
+        return isCageValid();
+    }
+    
+    private boolean isCageValid() {
+        return cage.stream().anyMatch(c -> c.getValue() == 0)
+                || cageTotal == cage.stream().mapToInt(AbstractCell::getValue).sum();
+    }
+}
